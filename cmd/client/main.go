78f1f65fa46c921fcc8b52203ce2cd79b6b79dd4
shaheerkt <shaheerkt123@users.noreply.github.com>
2026-06-01 17:27:50 +0530

    a lot of things has changed

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
+type NtfyResponse struct {
+	Event   string `json:"event"`
+	Message string `json:"message"`
+}
+
 func main() {
-	Port := ":8080"
+	Port := ":8081"
+	ClientTopic := "https://ntfy.sh/tcp-over-ntfy-client"
+	ServerTopic := "https://ntfy.sh/tcp-over-ntfy-server/json"
 
 	listener, err := net.Listen("tcp", Port)
 	if err != nil {
@@ -20,40 +30,102 @@ func main() {
 	fmt.Printf("Server is listening on Port %v...\n", Port)
 
 	for {
-		conn, err := listener.Accept()
+		localClientConnection, err := listener.Accept()
 		if err != nil {
 			log.Fatalf("Failed to accept connection: %v\n", err)
 			continue
 		}
-		go sendClientConnection(conn)
+		fmt.Printf("New connection from: %s\n", localClientConnection.RemoteAddr().String())
+
+		defer localClientConnection.Close()
+		go sendLocalClientDataToNtfy(localClientConnection, ServerTopic)
+		go recieveRemoteServerDataFromNtfy(localClientConnection, ClientTopic)
 	}
 }
 
-func sendClientConnection(conn net.Conn) {
-	ClientTopic := "https://ntfy.sh/tcp-over-ntfy-client"
-	defer conn.Close()
-	fmt.Printf("New connection from: %s\n", conn.RemoteAddr().String())
+func sendLocalClientDataToNtfy(conn net.Conn, ClientTopic string) {
 	connectionBuffer := make([]byte, 1024)
 	for {
 		n, err := conn.Read(connectionBuffer)
+		fmt.Printf("local client data: %v\n", string(connectionBuffer[:n]))
 		if n > 0 {
-			resp, postErr := http.Post(ClientTopic, "application/octet-stream",
-				bytes.NewReader(connectionBuffer[:n]))
+			resp, postErr := http.Post(ClientTopic, "application/octet-stream", strings.NewReader(fmt.Sprintf("%v", connectionBuffer[:n])))
 			if postErr != nil {
-				fmt.Printf("Error sending the data to ntfy: %v\n", err)
+				fmt.Printf("Error sending the local client data to ntfy: %v\n", err)
 			}
-			closeErr := resp.Body.Close()
-			if closeErr != nil {
+			if err := resp.Body.Close(); err != nil {
 				fmt.Printf("Error closing http Post: %v\n", err)
 			}
 		}
 		if err != nil {
 			if err == io.EOF {
-				fmt.Println("Client closed connection gracefully")
+				fmt.Println("local Client closed connection gracefully")
 			} else {
 				fmt.Printf("Network error occurred: %v\n", err)
 			}
 			return
 		}
 	}
+}
+
+func recieveRemoteServerDataFromNtfy(conn net.Conn, ServerTopic string) {
+	resp, err := http.Get(ServerTopic)
+	if err != nil {
+		log.Fatalf("Failed to connect to ntfy: %v\n", err)
+	}
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			fmt.Printf("Error closing ntfy connection: %v\n", err)
+		}
+	}()
+
+	fmt.Println("listening for events from ntfy...")
+
+	scanner := bufio.NewScanner(resp.Body)
+
+	for scanner.Scan() {
+		if len(scanner.Bytes()) == 0 {
+			continue
+		}
+
+		var ntfyResponse NtfyResponse
+		if err := json.Unmarshal(scanner.Bytes(), &ntfyResponse); err != nil {
+			fmt.Printf("Error parsing json from ntfy: %v\n", err)
+			continue
+		}
+
+		if ntfyResponse.Event == "message" {
+			ntfyServerDataBytes, err := asciiToByte(ntfyResponse.Message)
+			if err != nil {
+				fmt.Printf("Error converting data to Byte: %v\n", err)
+				continue
+			}
+			fmt.Printf("remote server data: %v\n", string(ntfyServerDataBytes))
+			if _, err := conn.Write(ntfyServerDataBytes); err != nil {
+				fmt.Printf("Failed to send data to client: %v\n", err)
+				continue
+			}
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Stream Error: %v\n", err)
+	}
+}
+
+func asciiToByte(ascii string) ([]byte, error) {
+	cleaned := strings.Trim(ascii, "[]")
+	fields := strings.Fields(cleaned)
+	result := make([]byte, len(fields))
+	for i, field := range fields {
+		val, err := strconv.Atoi(field)
+		if err != nil {
+			return nil, fmt.Errorf("error converting '%s' at index %d to uint8: %w", field, i, err)
+		}
+		if val < 0 || val > 255 {
+			return nil, fmt.Errorf("value '%d' at index %d out of uint8 range", val, i)
+		}
+
+		result[i] = byte(val)
+	}
+	return result, nil
 }
