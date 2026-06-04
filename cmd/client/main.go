e03592b686ceebc482de74bcacb5e9270de6dc55
shaheerkt <shaheerkt123@users.noreply.github.com>
2026-06-04 17:29:07 +0530

    encode my messages to base64 for more data density,
    handle multiple connections properly,
    
    parallalize a lot of things,
    
    give a little more naming stability,
    
    and remove main.go in the root directory that somehow spill from my other project
    
    but still doesnt work

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -20,7 +21,8 @@ type NtfyResponse struct {
 func main() {
 	Port := ":8081"
 	ClientTopic := "https://ntfy.sh/tcp-over-ntfy-client"
-	ServerTopic := "https://ntfy.sh/tcp-over-ntfy-server/json"
+	ServerTopic := "https://ntfy.sh/tcp-over-ntfy-server"
+	IndexTopic := "https://ntfy.sh/tcp-over-ntfy-index"
 
 	listener, err := net.Listen("tcp", Port)
 	if err != nil {
@@ -29,27 +31,58 @@ func main() {
 	defer listener.Close()
 	fmt.Printf("Server is listening on Port %v...\n", Port)
 
+	index := 0
 	for {
 		localClientConnection, err := listener.Accept()
 		if err != nil {
-			log.Fatalf("Failed to accept connection: %v\n", err)
+			fmt.Printf("Failed to accept connection: %v\n", err)
 			continue
 		}
+		http.Post(IndexTopic, "text/plain", strings.NewReader(strconv.Itoa(index)))
 		fmt.Printf("New connection from: %s\n", localClientConnection.RemoteAddr().String())
 
-		defer localClientConnection.Close()
-		go sendLocalClientDataToNtfy(localClientConnection, ServerTopic)
-		go recieveRemoteServerDataFromNtfy(localClientConnection, ClientTopic)
+		go handleConnection(localClientConnection,
+			fmt.Sprintf("%s-%d/json?cache=no", ServerTopic, index),
+			fmt.Sprintf("%s-%d", ClientTopic, index))
+
+		index++
 	}
 }
 
-func sendLocalClientDataToNtfy(conn net.Conn, ClientTopic string) {
+func handleConnection(localClientConnection net.Conn, listenTopic string, publishTopic string) {
+	done := make(chan struct{})
+
+	go func() {
+		recieveRemoteServerDataFromNtfy(localClientConnection, listenTopic)
+		select {
+		case <-done:
+			return
+		default:
+			close(done)
+		}
+	}()
+	go func() {
+		sendLocalClientDataToNtfy(localClientConnection, publishTopic)
+		select {
+		case <-done:
+			return
+		default:
+			close(done)
+		}
+	}()
+
+	<-done
+	localClientConnection.Close()
+}
+
+func sendLocalClientDataToNtfy(localClientConnection net.Conn, ClientTopic string) {
 	connectionBuffer := make([]byte, 1024)
 	for {
-		n, err := conn.Read(connectionBuffer)
+		n, err := localClientConnection.Read(connectionBuffer)
 		fmt.Printf("local client data: %v\n", string(connectionBuffer[:n]))
 		if n > 0 {
-			resp, postErr := http.Post(ClientTopic, "application/octet-stream", strings.NewReader(fmt.Sprintf("%v", connectionBuffer[:n])))
+			resp, postErr := http.Post(ClientTopic, "application/octet-stream",
+				strings.NewReader(base64.StdEncoding.EncodeToString(connectionBuffer[:n])))
 			if postErr != nil {
 				fmt.Printf("Error sending the local client data to ntfy: %v\n", err)
 			}
@@ -68,7 +101,7 @@ func sendLocalClientDataToNtfy(conn net.Conn, ClientTopic string) {
 	}
 }
 
-func recieveRemoteServerDataFromNtfy(conn net.Conn, ServerTopic string) {
+func recieveRemoteServerDataFromNtfy(localClientConnection net.Conn, ServerTopic string) {
 	resp, err := http.Get(ServerTopic)
 	if err != nil {
 		log.Fatalf("Failed to connect to ntfy: %v\n", err)
@@ -84,48 +117,34 @@ func recieveRemoteServerDataFromNtfy(conn net.Conn, ServerTopic string) {
 	scanner := bufio.NewScanner(resp.Body)
 
 	for scanner.Scan() {
-		if len(scanner.Bytes()) == 0 {
-			continue
-		}
-
-		var ntfyResponse NtfyResponse
-		if err := json.Unmarshal(scanner.Bytes(), &ntfyResponse); err != nil {
-			fmt.Printf("Error parsing json from ntfy: %v\n", err)
-			continue
-		}
-
-		if ntfyResponse.Event == "message" {
-			ntfyServerDataBytes, err := asciiToByte(ntfyResponse.Message)
-			if err != nil {
-				fmt.Printf("Error converting data to Byte: %v\n", err)
-				continue
-			}
-			fmt.Printf("remote server data: %v\n", string(ntfyServerDataBytes))
-			if _, err := conn.Write(ntfyServerDataBytes); err != nil {
-				fmt.Printf("Failed to send data to client: %v\n", err)
-				continue
-			}
-		}
+		go sendScannerDataTolocalClient(scanner, localClientConnection)
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Printf("Stream Error: %v\n", err)
 	}
 }
 
-func asciiToByte(ascii string) ([]byte, error) {
-	cleaned := strings.Trim(ascii, "[]")
-	fields := strings.Fields(cleaned)
-	result := make([]byte, len(fields))
-	for i, field := range fields {
-		val, err := strconv.Atoi(field)
-		if err != nil {
-			return nil, fmt.Errorf("error converting '%s' at index %d to uint8: %w", field, i, err)
-		}
-		if val < 0 || val > 255 {
-			return nil, fmt.Errorf("value '%d' at index %d out of uint8 range", val, i)
-		}
-
-		result[i] = byte(val)
+func sendScannerDataTolocalClient(scanner *bufio.Scanner, localClientConnection net.Conn) {
+	if len(scanner.Bytes()) == 0 {
+		return
 	}
-	return result, nil
+
+	var ntfyResponse NtfyResponse
+	if err := json.Unmarshal(scanner.Bytes(), &ntfyResponse); err != nil {
+		fmt.Printf("Error parsing json from ntfy: %v\n", err)
+		return
+	}
+
+	if ntfyResponse.Event == "message" {
+		ntfyServerDataBytes, err := base64.StdEncoding.DecodeString(ntfyResponse.Message)
+		if err != nil {
+			fmt.Printf("Error converting base64 to Bytes: %v\n", err)
+			return
+		}
+		fmt.Printf("remote server data: %v\n", string(ntfyServerDataBytes))
+		if _, err := localClientConnection.Write(ntfyServerDataBytes); err != nil {
+			fmt.Printf("Failed to send data to local client: %v\n", err)
+			return
+		}
+	}
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"bufio"
-	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -19,9 +20,101 @@ type NtfyResponse struct {
 
 func main() {
 	Port := ":8080"
-	ClientTopic := "https://ntfy.sh/tcp-over-ntfy-client/json"
+	ClientTopic := "https://ntfy.sh/tcp-over-ntfy-client"
 	ServerTopic := "https://ntfy.sh/tcp-over-ntfy-server"
+	IndexTopic := "https://ntfy.sh/tcp-over-ntfy-index"
 
+	resp, err := http.Get(fmt.Sprintf("%s/json?cache=no", IndexTopic))
+	if err != nil {
+		fmt.Printf("Error connecting to ntfy index: %v\n", err)
+		return
+	}
+	defer resp.Body.Close()
+	scanner := bufio.NewScanner(resp.Body)
+	for scanner.Scan() {
+		if len(scanner.Bytes()) == 0 {
+			continue
+		}
+
+		var ntfyResponse NtfyResponse
+
+		if err := json.Unmarshal(scanner.Bytes(), &ntfyResponse); err != nil {
+			fmt.Printf("Error parsing json from ntfy: %v\n", err)
+			continue
+		}
+
+		if ntfyResponse.Event == "message" {
+			index := ntfyResponse.Message
+			go handleConnection(
+				fmt.Sprintf("%s-%s/json?cache=no", ClientTopic, index),
+				fmt.Sprintf("%s-%s", ServerTopic, index),
+				Port)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Stream Error: %v\n", err)
+	}
+}
+
+func handleConnection(listenTopic string, publishTopic string, port string) {
+	conn, err := net.DialTimeout("tcp", fmt.Sprintf("localhost%s", port), time.Second*5)
+	if err != nil {
+		log.Fatalf("Failed to connect to port %s: %v\n", port, err)
+	}
+	fmt.Println("successfully connected to port", port)
+
+	done := make(chan struct{})
+
+	go func() {
+		sendLocalServerDataToNtfy(conn, publishTopic)
+		select {
+		case <-done:
+			return
+		default:
+			close(done)
+		}
+	}()
+	go func() {
+		recieveRemoteClientDataFromNtfy(conn, listenTopic)
+		select {
+		case <-done:
+			return
+		default:
+			close(done)
+		}
+	}()
+	<-done
+	conn.Close()
+}
+
+func sendLocalServerDataToNtfy(conn net.Conn, ServerTopic string) {
+	connectionBuffer := make([]byte, 1024)
+	for {
+		n, err := conn.Read(connectionBuffer)
+		if n > 0 {
+			fmt.Printf("local server data: %v\n", string(connectionBuffer[:n]))
+			resp, postErr := http.Post(ServerTopic, "application/octet-stream",
+				strings.NewReader(base64.StdEncoding.EncodeToString(connectionBuffer[:n])))
+			if postErr != nil {
+				fmt.Printf("Error sending data to ntfy: %v\n", err)
+			}
+			if err := resp.Body.Close(); err != nil {
+				fmt.Printf("Error closing http Post: %v\n", err)
+			}
+			fmt.Println("successfully sent local server data to ntfy")
+		}
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Server closed connection gracefully")
+			} else {
+				fmt.Printf("Network error occurred: %v\n", err)
+			}
+			return
+		}
+	}
+}
+
+func recieveRemoteClientDataFromNtfy(conn net.Conn, ClientTopic string) {
 	resp, err := http.Get(ClientTopic)
 	if err != nil {
 		log.Fatalf("Failed to connect to ntfy: %v\n", err)
@@ -34,15 +127,6 @@ func main() {
 
 	fmt.Println("listening for events from ntfy...")
 	scanner := bufio.NewScanner(resp.Body)
-
-	conn, err := net.DialTimeout("tcp", fmt.Sprintf("localhost%s", Port), time.Second*5)
-	if err != nil {
-		log.Fatalf("Failed to connect to port %s: %v\n", Port, err)
-	}
-	defer conn.Close()
-
-	go sendServerConnectiontoNtfy(conn, ServerTopic)
-
 	for scanner.Scan() {
 		if len(scanner.Bytes()) == 0 {
 			continue
@@ -55,37 +139,20 @@ func main() {
 		}
 
 		if ntfyResponse.Event == "message" {
-			if _, err := conn.Write([]byte(ntfyResponse.Message)); err != nil {
+			convertedToByte, err := base64.StdEncoding.DecodeString(ntfyResponse.Message)
+			if err != nil {
+				fmt.Printf("Error converting base64 to bytes: %v\n", err)
+				continue
+			}
+			n, err := conn.Write(convertedToByte)
+			if err != nil {
 				fmt.Printf("Failed to send data to local server: %v\n", err)
 			}
+			fmt.Printf("Message length: %v, Sent: %v\n", len([]byte(ntfyResponse.Message)), n)
+			fmt.Printf("remote client request: %v\n", (ntfyResponse.Message))
 		}
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Printf("Stream Error: %v\n", err)
-	}
-}
-
-func sendServerConnectiontoNtfy(conn net.Conn, ServerTopic string) {
-	connectionBuffer := make([]byte, 1024)
-	for {
-		n, err := conn.Read(connectionBuffer)
-		if n > 0 {
-			resp, postErr := http.Post(ServerTopic, "application/octet-stream",
-				bytes.NewReader(connectionBuffer[:n]))
-			if postErr != nil {
-				fmt.Printf("Error sending the data to ntfy: %v\n", err)
-			}
-			if err := resp.Body.Close(); err != nil {
-				fmt.Printf("Error closing http Post: %v\n", err)
-			}
-		}
-		if err != nil {
-			if err == io.EOF {
-				fmt.Println("Server closed connection gracefully")
-			} else {
-				fmt.Printf("Network error occurred: %v\n", err)
-			}
-			return
-		}
 	}
 }
